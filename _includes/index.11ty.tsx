6ce7078ea5108699b7758b316db9a7c5e33c96c6
pauleveritt <paul.everitt@jetbrains.com>
2023-05-08 20:19:44 -0400

    Can't get h right.

--- _includes/index.11ty.tsx
+++ _includes/index.11ty.tsx
@@ -1,5 +1,5 @@
 // noinspection ES6UnusedImports
-import h from "vhtml";
+import { h } from "preact";
 
 export const data = {
   title: "Eleventy 11ty.js Extensions",
@@ -14,25 +14,5 @@ interface Data {
 }
 
 export function render(this: Context, { title }: Data) {
-//   this.log(title);
-//   return (
-//     <html lang="en">
-//       <head>
-//         <title>{title}</title>
-//       </head>
-//       <body>
-//         <main className="container">
-//           <h1>{title}</h1>
-//           <p>
-//             Example repo to show the value of{" "}
-//             <a href="https://github.com/11ty/eleventy/issues/2248">
-//               11ty/eleventy#2248
-//             </a>
-//             .
-//           </p>
-//         </main>
-//       </body>
-//     </html>
-//   );
-  return <div>Hello</div>
+  return <div>Hello</div>;
 }
