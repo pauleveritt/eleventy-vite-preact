a75a24b6a39a1a028c78f9aa4c080a175c181132
pauleveritt <paul.everitt@jetbrains.com>
2023-05-11 20:33:19 -0400

    Try replacing the vhtml h function to bind `this`.

--- _includes/index.11ty.tsx
+++ _includes/index.11ty.tsx
@@ -1,8 +1,21 @@
-// @jsxImportSource preact
-// @jsxRuntime automatic
+import h from "../ghtml";
 
-export function IndexView() {
-  return <h1 id="target">Hello</h1>;
+type HeadingProps = {
+  title: string;
+};
+
+function Heading({ title }: HeadingProps) {
+  return <h1 id="target">Hello {title}</h1>;
+}
+
+type IndexViewProps = {
+  title: string;
+};
+
+export function IndexView(this: any, { title }: IndexViewProps) {
+  const x = <Heading title={title} />;
+  x.bind(this);
+  return x;
 }
 
 export const render = IndexView;
